02b80c093412aa5b19835f1a147f0351fc0c1c5f
ysf <yuansssf@gmail.com>
2019-11-01 16:26:40 +0800

    存储用户自定义的课表

---
+++ app/src/main/java/com/example/daidaijie/syllabusapplication/syllabus/CustomizeLessonDataBase.java
@@ -0,0 +1,29 @@
+package com.example.daidaijie.syllabusapplication.syllabus.customizelesson;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.support.annotation.Nullable;
+
+/**
+ * yuan
+ * 2019/11/1
+ **/
+public class CustomizeLessonDataBase extends SQLiteOpenHelper {
+
+    public CustomizeLessonDataBase(@Nullable Context context, @Nullable String name, @Nullable SQLiteDatabase.CursorFactory factory, int version) {
+        super(context, name, factory, version);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase sqLiteDatabase) {
+        // TODO: 2019/11/1 创建一张数据表
+        String sql = "create table customize_lesson(years varchar(64), semester varchar(64), id varchar(64), name varchar(64), classroom varchar(64), week varchar(64), detail varchar(64))";
+        sqLiteDatabase.execSQL(sql);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
+
+    }
+}
